dbb62774b523eefec3f6447bfc918f8f715839d7
Simplegram <nkgnicholas@gmail.com>
2025-05-09 22:44:14 +0700

    Add: context for modifying items

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,35 @@
 import { TravelContext } from "@/context/PageContext";
-import { AuthProvider } from "@/provider/AuthProvider";
 import { DataItem } from "@/src/types/Travels";
-import { FontAwesome } from "@expo/vector-icons";
+import Icon from 'react-native-vector-icons/FontAwesome6'
 import { Tabs } from "expo-router";
 import { useState } from "react";
 
 const TabsLayout = () => {
   const [selectedItem, setSelectedItem] = useState<DataItem | undefined>(undefined);
+  const [selectedModification, setSelectedModification] = useState<string | undefined>(undefined)
 
   return (
-    <TravelContext.Provider value={{ selectedItem, setSelectedItem }}>
+    <TravelContext.Provider value={{ selectedItem, selectedModification, setSelectedItem, setSelectedModification }}>
       <Tabs>
           <Tabs.Screen 
               name="mainMenu" 
               options={{ 
                   title: "Home", 
                   headerShown: false,
-                  tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+                  tabBarIcon: ({ color }) => <Icon size={24} name="house" color={color} />,
+                  tabBarStyle: {
+                    height: 60
+                  },
+                  tabBarLabelStyle: {
+                    fontSize: 13
+                  }
+          }} />
+          <Tabs.Screen 
+              name="addTravel" 
+              options={{ 
+                  title: "Add", 
+                  headerShown: false,
+                  tabBarIcon: ({ color }) => <Icon size={24} name="square-plus" color={color} />,
                   tabBarStyle: {
                     height: 60
                   },
@@ -27,9 +40,9 @@ const TabsLayout = () => {
           <Tabs.Screen 
               name="addPage" 
               options={{ 
-                  title: "Add", 
+                  title: "Modify", 
                   headerShown: false,
-                  tabBarIcon: ({ color }) => <FontAwesome size={28} name="plus" color={color} />,
+                  tabBarIcon: ({ color }) => <Icon size={24} name="pen-to-square" color={color} />,
                   tabBarStyle: {
                     height: 60
                   },
@@ -42,7 +55,21 @@ const TabsLayout = () => {
               options={{ 
                   title: "Add", 
                   headerShown: false,
-                  tabBarIcon: ({ color }) => <FontAwesome size={28} name="plus" color={color} />,
+                  tabBarIcon: ({ color }) => <Icon size={24} name="plus" color={color} />,
+                  tabBarStyle: {
+                    height: 60
+                  },
+                  tabBarLabelStyle: {
+                    fontSize: 13
+                  },
+              href: null
+          }} />
+          <Tabs.Screen 
+              name="dataList" 
+              options={{ 
+                  title: "Add", 
+                  headerShown: false,
+                  tabBarIcon: ({ color }) => <Icon size={24} name="plus" color={color} />,
                   tabBarStyle: {
                     height: 60
                   },
