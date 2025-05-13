872958aa78cabc5d82f912c80eb9980d0f729ac5
Simplegram <nkgnicholas@gmail.com>
2025-05-13 11:58:32 +0700

    Add: add direction modal

---
+++ components/modal/addModal/AddDirectionModal.tsx
@@ -0,0 +1,95 @@
+import Button from "@/components/BaseButton"
+import useLoading from "@/hooks/useLoading"
+import { AddableDirection } from "@/src/types/AddableTravels"
+import { BaseModalContentProps } from "@/src/types/ModalContentProps"
+import { useState } from "react"
+import { StyleSheet, Text, TextInput, View } from "react-native"
+
+export default function AddDirectionModal({ onCancel, onSubmit }: BaseModalContentProps) {
+    const [direction, setDirection] = useState<AddableDirection>({ name: undefined })
+
+    const { loading } = useLoading()
+
+    const handleOnSubmit = () => {
+        onSubmit(direction);
+    };
+
+    return (
+        <View style={styles.container}>
+            {loading ? (
+                <Text style={styles.label}>Loading...</Text>
+            ) : (
+                <>
+                    <View style={styles.inputContainer}>
+                        <Text style={styles.label}>Name:</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Direction name..."
+                            value={direction.name}
+                            onChangeText={text => (setDirection({ ...direction, "name": text }))}
+                        />
+                    </View>
+
+                    <View style={buttonStyles.buttonRow}>
+                        <Button title='Cancel' color='#E0E0E0' onPress={onCancel} style={buttonStyles.cancelButton} textStyle={buttonStyles.cancelButtonText}></Button>
+                        <Button title='Edit Route' color='#0284f5' onPress={handleOnSubmit} style={buttonStyles.addButton} textStyle={buttonStyles.addButtonText}></Button>
+                    </View>
+                </>
+            )}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {},
+    inputContainer: {
+        gap: 10,
+        flexDirection: 'column',
+        paddingVertical: 10,
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 8
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        paddingBottom: 12,
+        fontSize: 16,
+    },
+})
+
+const buttonStyles = StyleSheet.create({
+    buttonRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 10,
+        gap: 10,
+    },
+    addButton: {
+        backgroundColor: '#1E88E5',
+        paddingVertical: 12,
+        borderRadius: 8,
+        flex: 1,
+        alignItems: 'center',
+    },
+    addButtonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '600',
+    },
+    cancelButton: {
+        paddingVertical: 12,
+        borderRadius: 8,
+        flex: 1,
+        alignItems: 'center',
+    },
+    cancelButtonText: {
+        color: '#000',
+        fontSize: 16,
+        fontWeight: '600',
+    },
+})
