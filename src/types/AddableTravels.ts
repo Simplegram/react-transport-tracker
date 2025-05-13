9315b8efed25962a62d40e71e3619b008bc4da38
Simplegram <nkgnicholas@gmail.com>
2025-05-13 11:52:49 +0700

    Add: addable data types

---
+++ src/types/AddableTravels.ts
@@ -0,0 +1,18 @@
+interface AddableDirection {
+    name: string | undefined
+}
+
+interface AddableVehicleType {
+    name: string | undefined,
+    icon_id: number | undefined,
+}
+
+interface AddableIconType {
+    name: string | undefined
+}
+
+export {
+    AddableDirection,
+    AddableVehicleType,
+    AddableIconType
+}
