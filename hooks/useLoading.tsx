1606665bc682700fc163b06445f04b3b349f22bc
Simplegram <nkgnicholas@gmail.com>
2025-05-14 16:40:14 +0700

    Add: toggle loading hook

--- hooks/useLoading.tsx
+++ hooks/useLoading.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function useLoading(timeout: number = 500) {
+export function useLoading(timeout: number = 500) {
     const [loading, setLoading] = useState<boolean>(true)
 
     useEffect(() => {
@@ -11,5 +11,20 @@ export default function useLoading(timeout: number = 500) {
 
     return {
         loading
+    }
+}
+
+export function useToggleLoading(timeout: number = 500, defaultValue: boolean = false) {
+    const [loading, setLoading] = useState<boolean>(defaultValue)
+
+    const toggleLoading = () => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, timeout);
+    }
+
+    return {
+        loading, setLoading, toggleLoading
     }
 }
