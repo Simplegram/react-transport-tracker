41f4e09d3404977e4df2117e4b444fa5757c486e
Simplegram <nkgnicholas@gmail.com>
2025-05-18 21:49:59 +0700

    Add: user location hook

---
+++ hooks/useLocation.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+import * as Location from 'expo-location';
+import { Alert } from "react-native";
+
+export default function useLocation() {
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+
+    async function getCurrentLocation() {
+
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            Alert.alert('Location access denied', 'Permission to access location was denied');
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+    }
+
+    useEffect(() => {
+        getCurrentLocation();
+    }, []);
+
+    return {
+        location
+    }
+}
