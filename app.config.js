7ce2f558fcc0ebbcf02e3d90311c7eb700946e39
Simplegram <nkgnicholas@gmail.com>
2025-05-19 12:24:47 +0700

    Change: bump version

--- app.config.js
+++ app.config.js
@@ -30,7 +30,7 @@ export default {
     expo: {
         name: getAppName(),
         slug: "TransportTracker",
-        version: "1.3.4b2",
+        version: "1.4.0",
         icon: "./assets/images/icon.png",
         ios: {
             bundleIdentifier: getUniqueIdentifier(),
