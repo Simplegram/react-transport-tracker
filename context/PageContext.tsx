ee11760f8348c1625c06337840f1518ae655236b
Simplegram <nkgnicholas@gmail.com>
2025-05-28 22:13:36 +0700

    Remove: unused context

--- context/PageContext.tsx
+++ context/PageContext.tsx
@@ -4,10 +4,8 @@ import { createContext, PropsWithChildren, useContext, useState } from "react"
 interface TravelContextValue {
     selectedItem: any | undefined
     setSelectedItem: (item: any | undefined) => void
-    selectedModification: string | undefined
-    setSelectedModification: (modification: string | undefined) => void
     selectedTravelItems: DataItem[] | undefined
-    setSelectedTravelItems: (travels: DataItem[] | undefined) => void
+    setSelectedTravelItems: (items: DataItem[] | undefined) => void
 }
 
 export const TravelContext = createContext<TravelContextValue | undefined>(undefined)
@@ -15,14 +13,11 @@ export const TravelContext = createContext<TravelContextValue | undefined>(undef
 export const TravelProvider = ({ children }: PropsWithChildren) => {
     const [selectedItem, setSelectedItem] = useState<DataItem | undefined>(undefined)
     const [selectedTravelItems, setSelectedTravelItems] = useState<DataItem[] | undefined>(undefined)
-    const [selectedModification, setSelectedModification] = useState<string | undefined>(undefined)
 
     return (
         <TravelContext.Provider value={{
             selectedItem,
             setSelectedItem,
-            selectedModification,
-            setSelectedModification,
             selectedTravelItems,
             setSelectedTravelItems
         }}>
