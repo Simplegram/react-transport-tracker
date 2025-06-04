60e777f95282382029e52b22b8adcd0cb30688cb
Simplegram <nkgnicholas@gmail.com>
2025-06-04 18:35:36 +0700

    fix: type error

--- components/modal/addModal/AddLapModal.tsx
+++ components/modal/addModal/AddLapModal.tsx
@@ -40,7 +40,7 @@ export default function AddLapModal({ stops, isModalVisible, onClose, onSelect }
 
     const mapRef = useRef(null)
 
-    const [lap, setLap] = useState<AddableLap>({ time: undefined, lat: null, lon: null, stop_id: null, note: null })
+    const [lap, setLap] = useState<AddableLap>({ id: Crypto.randomUUID(), time: undefined, lat: undefined, lon: undefined, stop_id: undefined, note: undefined })
 
     const [showDatetimePicker, setShowDatetimePicker] = useState(false)
 
@@ -67,10 +67,10 @@ export default function AddLapModal({ stops, isModalVisible, onClose, onSelect }
             const currentTime = new Date().toISOString()
             const formattedTime = formatLapTimeDisplay(currentTime)
 
-            setLap({ ...lap, id: Crypto.randomUUID(), time: formattedTime, lon: null, lat: null, stop_id: null, note: null })
+            setLap({ ...lap, time: formattedTime, lon: undefined, lat: undefined, stop_id: undefined, note: undefined })
 
             return () => {
-                setLap({ ...lap, id: Crypto.randomUUID(), time: formattedTime, lon: null, lat: null, stop_id: null, note: null })
+                setLap({ ...lap, id: Crypto.randomUUID(), time: formattedTime, lon: undefined, lat: undefined, stop_id: undefined, note: undefined })
             }
         }, [isModalVisible])
     )
