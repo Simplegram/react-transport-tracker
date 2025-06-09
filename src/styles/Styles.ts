9474f9a5e3719fd42397d984d27b2d6d23cb2d7c
Simplegram <nkgnicholas@gmail.com>
2025-06-09 21:30:57 +0700

    refactor: remove unused styles

--- src/styles/Styles.ts
+++ src/styles/Styles.ts
@@ -1,26 +1,4 @@
 import { colors } from "@/src/const/color"
-import { StyleSheet } from "react-native"
-
-const lightStyles = StyleSheet.create({
-    icon: {
-        alignItems: 'center',
-    },
-})
-
-export const styles = {
-    light: lightStyles,
-    dark: StyleSheet.create({
-        icon: {
-            ...lightStyles.icon,
-            color: colors.white_300,
-        }
-    })
-}
-
-export const dividerStyles = {
-    light: colors.black,
-    dark: colors.white_300,
-}
 
 export const statusBarStyles = {
     light: colors.white_100,
