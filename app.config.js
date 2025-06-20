554d6d071e0b28a9e6783da106344cbc3e17cebc
Simplegram <nkgnicholas@gmail.com>
2025-06-20 18:51:02 +0700

    chore: bump version to 1.6.0-rc.7

--- app.config.js
+++ app.config.js
@@ -30,7 +30,7 @@ export default {
     expo: {
         name: getAppName(),
         slug: "TransportTracker",
-        version: "1.6.0-rc.6",
+        version: "1.6.0-rc.7",
         icon: "./assets/images/icon.png",
         ios: {
             bundleIdentifier: getUniqueIdentifier(),
